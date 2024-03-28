8eb8611fb3354b76bdcddd3ed6784ab711159b28
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-03-28 22:18:54 +0200

    add planets

--- script.js
+++ script.js
@@ -16,4 +16,24 @@ function mySolarSystem() {
     ellipse(680, 300, 780, 270);
     ellipse(680, 300, 1000, 350);
     ellipse(680, 300, 1200, 450);
+     //planeta1
+    fill("gray");
+    circle(1140, 240,100,100);
+
+    //planeta2
+    fill("lightblue");
+    circle(1200, 410,100,100);
+
+    //planeta3
+    fill("lightyellow");
+    circle(548,250,20,20);
+
+    //planeta4
+    fill("lightgreen");
+    circle(430,250,35,35);
+
+    //planeta5
+    fill("lightorange");
+    circle(340,235,48,48);
+
 }
